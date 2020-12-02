5a554b2931f5d61099c76aed09cbeae78c6dbfaa
Karaay <karaaykaraogul@gmail.com>
2020-12-03 00:28:50 +0300

    [Karaay] Db baglandi

---
+++ Proje/Data/MuslukDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proje.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Data
+{
+    public class MuslukDbContext : DbContext
+    {
+        public MuslukDbContext(DbContextOptions<MuslukDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ Proje/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Models
+{
+    public class User
+    {
+        public int userID { get; set; }//PK
+        public string userName { get; set; }
+        public string userPass { get; set; }
+        public string userMail { get; set; }
+        public int userWallet { get; set; }
+        public int userIsAdmin { get; set; }
+        public string userImg { get; set; }
+
+    }
+}
