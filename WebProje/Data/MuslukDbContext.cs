6705d3eb9c55ce9294632002ded093c81daeed30
Karaay <karaaykaraogul@gmail.com>
2020-12-09 17:45:53 +0300

    [Karaay]Database dosyası eklendi. Register Login düzenlendi

---
+++ WebProje/Data/MuslukDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WebProje.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProje.Data
+{
+    public class MuslukDbContext : IdentityDbContext
+    {
+        public MuslukDbContext(DbContextOptions<MuslukDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
