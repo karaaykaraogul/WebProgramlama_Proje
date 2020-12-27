e0250c590fc66be3796c67046524b7eda10b4611
Karaay <karaaykaraogul@gmail.com>
2020-12-27 16:42:47 +0300

    [Karaay] Store Data getirme ve Game sayfaları

--- WebProje/Controllers/GameController.cs
+++ WebProje/Controllers/GameController.cs
@@ -1,19 +1,164 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebProje.Data;
 using WebProje.Models;
 
-namespace WebProje.Controllers
+namespace WebProje
 {
     public class GameController : Controller
     {
-        public IActionResult Page()
+        private readonly MuslukDbContext _context;
+
+        public GameController(MuslukDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Game
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Game.ToListAsync());
+        }
+
+        public async Task<IActionResult> Page()
+        {
+            
+            var Oyun = from a in _context.Game
+                       where a.gameID == 1
+                       select a;
+            
+            return View(Oyun);
+        }
+
+
+        // GET: Game/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game
+                .FirstOrDefaultAsync(m => m.gameID == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // GET: Game/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Game/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("gameID,gameName,gamePrice,gameGenre,gameRD,gameSold,gameDesc_EN,gameDesc_TR")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(game);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+
+        // GET: Game/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Game/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("gameID,gameName,gamePrice,gameGenre,gameRD,gameSold,gameDesc_EN,gameDesc_TR")] Game game)
+        {
+            if (id != game.gameID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(game);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GameExists(game.gameID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+
+        // GET: Game/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game
+                .FirstOrDefaultAsync(m => m.gameID == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // POST: Game/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var game = await _context.Game.FindAsync(id);
+            _context.Game.Remove(game);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GameExists(int id)
+        {
+            return _context.Game.Any(e => e.gameID == id);
         }
     }
 }
